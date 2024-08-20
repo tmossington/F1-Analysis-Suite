215b0dceb72c39c37cade03602e64b441ce4221c
Tom Mossington <tmossington@gmail.com>
2024-08-20 11:03:53 -0400

    Added dependancies

---
+++ main.py
@@ -0,0 +1,9 @@
+import fastf1 as ff1
+from fastf1 import plotting
+from matplotlib import pyplot as plt
+from matplotlib.pyplot import figure
+from matplotlib.collections import LineCollection
+from matplotlib import cm 
+import numpy as np
+import pandas as pd
+
